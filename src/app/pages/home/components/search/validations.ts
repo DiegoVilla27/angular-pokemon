4d16b0a8e7bd973d4ae6e682f5b306454efba79c
Diego Villa <dvillanueva@sdi.es>
2024-06-18 19:02:37 +0200

    feat: Update error message component

--- src/app/pages/home/components/search/validations.ts
+++ src/app/pages/home/components/search/validations.ts
@@ -1,4 +1,6 @@
-export const validations = {
+import { IErrorMsg } from "../../../../components/error-msg/error-msg.component";
+
+export const validations: IErrorMsg = {
   query: [
     {
       type: "maxlength",
